397168c7142ee3cf9cba50766a3ee427ccb2c944
Rpeche-pk <pecheaparcana1998@gmail.com>
2023-10-08 17:47:27 -0500

    feat(backend)🚀: configuration, parameters, project dependencies

---
+++ cashier-backend/src/main/java/com/nocountry/app/cashier/persistance/entity/Customer.java
@@ -0,0 +1,11 @@
+package com.nocountry.app.cashier.persistance.entity;
+
+/**
+ * @author ROMULO
+ * @package com.nocountry.app.cashier.persistance.entity.listener
+ * @license Lrpa, zephyr cygnus
+ * @since 8/10/2023
+ */
+
+public class Customer {
+}
