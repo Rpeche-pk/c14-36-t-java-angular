361b2d7928659b89fabe61c9fdbccfdf3b32f57f
Dexametasona <machicadogomezalexander@gmail.com>
2023-10-09 10:08:02 -0500

    adding structure and styles to help page

--- cashier-app/src/app/app-routing.module.ts
+++ cashier-app/src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/Pages/home/home.component';
 import { LoginComponent } from './component/Pages/login/login.component';
+import { HelpComponent } from './component/Pages/helpPages/help/help.component';
+import { HelpRequestComponent } from './component/Pages/helpPages/help-request/help-request.component';
+import { HelpResponseComponent } from './component/Pages/helpPages/help-response/help-response.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
-  {path: 'login',component: LoginComponent }
+  {path: 'login',component: LoginComponent },
+  {path:'help', component:HelpComponent, children:[
+    {path:'', redirectTo:'/helpReq', pathMatch:'full'},
+    {path:'helpReq', component:HelpRequestComponent},
+    {path:'helpRes', component:HelpResponseComponent}
+  ]}
 ];
 
 @NgModule({
---
+++ cashier-app/src/app/component/Pages/helpPages/help-response/help-response.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-response',
+  templateUrl: './help-response.component.html',
+  styleUrls: ['./help-response.component.scss']
+})
+export class HelpResponseComponent {
+
+}
