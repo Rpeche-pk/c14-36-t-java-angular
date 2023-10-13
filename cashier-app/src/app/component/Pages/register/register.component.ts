22a005099619d61edfbb7987701465715d150c7b
Dexametasona <machicadogomezalexander@gmail.com>
2023-10-13 10:29:11 -0500

    style: improving the style to login and registration pages

--- cashier-app/src/app/component/Pages/register/register.component.ts
+++ cashier-app/src/app/component/Pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
-
+  constructor(private router : Router){}
+  toLogin(){
+    this.router.navigate(['login']);
+  }
 }
