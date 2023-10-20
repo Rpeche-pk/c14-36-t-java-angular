ffdcf14d0a50d8ab67ac59f7955f4c59b2056f38
Raul Gomez <gomez.raul.1978.rg@gmail.com>
2023-10-20 20:30:38 -0300

    fix

--- cashier-backend/src/main/java/com/nocountry/cashier/util/Constant.java
+++ cashier-backend/src/main/java/com/nocountry/cashier/util/Constant.java
@@ -12,6 +12,7 @@ public final class Constant {
     public static final String RESOURCE_AUTH= "/auth";
     public static final String RESOURCE_REGISTER= "/register";
     public static final String RESOURCE_USER= "/customers";
+
     public static final String RESOURCE_IMAGE= "/images";
     public static final String RESOURECE_TRANSACTION= "/transactions";
 
