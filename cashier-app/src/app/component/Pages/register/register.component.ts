011d6cdbc850362c03ec645f0f98da6c33bc2f1a
Dexametasona <machicadogomezalexander@gmail.com>
2023-10-25 11:34:40 -0500

    feat:adding validation and stiles to register page

--- cashier-app/src/app/component/Pages/register/register.component.ts
+++ cashier-app/src/app/component/Pages/register/register.component.ts
@@ -1,19 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/User.interface';
 import { UserService } from 'src/app/services/user.service';
-
+import {
+  birthValidator,
+  dniValidator,
+  emailValidator,
+  locateValidator,
+  passValidator,
+  phoneValidator,
+  repeatPassValidator,
+  textValidator,
+} from 'src/app/CustomValidator/customValidator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  eyeStatus = false;
+  eyeStatus2 = false;
+
+  repeatPass = false;
   created: boolean = false;
-  user!: FormGroup<User|any>;
-  users!: User[];
+  user!: FormGroup<User | any>;
+  // users!: User[];
 
   constructor(
     private userService: UserService,
@@ -22,24 +40,22 @@ export class RegisterComponent implements OnInit {
   ) {}
   ngOnInit(): void {
     this.user = this.initForm();
-    this.getAllData();
+    // this.getAllData();
   }
   toLogin() {
     this.router.navigate(['login']);
   }
 
-  
-
   onSubmit(): void {
     const userData: any = this.user.value;
     this.userService.addNewUser(userData).subscribe(
-      (response:any) => {
-        const message:string = response.data.message;
+      (response: any) => {
+        const message: string = response.data.message;
         if (message == 'Se registró correctamente') {
           this.router.navigate(['/login']);
         }
         console.log(message);
-        
+
         this.created = true;
       },
       (error) => {
@@ -48,23 +64,27 @@ export class RegisterComponent implements OnInit {
     );
   }
 
-  getAllData() {
-    this.userService.getUsers().subscribe(users => {
-      this.users = users;
-      console.log(this.users)
-    })
-  }
+  // getAllData() {
+  //   this.userService.getUsers().subscribe(users => {
+  //     this.users = users;
+  //     console.log(this.users)
+  //   })
+  // }
   initForm(): FormGroup {
     return this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      password2: ['', [Validators.required, Validators.minLength(8)]],
-      name: ['', [Validators.required]],
-      lastName: ['', [Validators.required]],
-      phone: ['', [Validators.required, Validators.minLength(7)]],
-      address: ['', [Validators.required]],
-      birthDate: ['', [Validators.required, Validators.maxLength(4)]],
-      dni: ['', [Validators.required, Validators.maxLength(8)]]
+      email: ['', [Validators.required, emailValidator]],
+      password: ['', [Validators.required, passValidator]],
+      password2: ['', [Validators.required, repeatPassValidator]],
+      name: ['', [Validators.required, textValidator]],
+      lastName: ['', [Validators.required, textValidator]],
+      phone: ['', [Validators.required, phoneValidator]],
+      address: ['', [Validators.required, locateValidator]],
+      birthDate: ['', [Validators.required, birthValidator]],
+      dni: ['', [Validators.required, dniValidator]],
     });
   }
+  validateField(fieldName: string) {
+    this.user.get(fieldName)?.updateValueAndValidity();
+  }
+
 }
